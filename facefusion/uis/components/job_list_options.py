2c45158ac8fd32f76b0b9ead0c200ec6b1c0d137
kintopna <kintopna@gmail.com>
2025-08-08 16:50:49 -0500

    Update job_list_options.py

--- facefusion/uis/components/job_list_options.py
+++ facefusion/uis/components/job_list_options.py
@@ -2,12 +2,12 @@ from typing import List, Optional
 
 import gradio
 
-import facefusion.choices
-from facefusion import state_manager, wording
-from facefusion.common_helper import get_first
-from facefusion.jobs import job_manager
-from facefusion.types import JobStatus
-from facefusion.uis.core import register_ui_component
+import facfusione.choices
+from facfusione import state_manager, wording
+from facfusione.common_helper import get_first
+from facfusione.jobs import job_manager
+from facfusione.types import JobStatus
+from facfusione.uis.core import register_ui_component
 
 JOB_LIST_JOB_STATUS_CHECKBOX_GROUP : Optional[gradio.CheckboxGroup] = None
 
@@ -16,11 +16,11 @@ def render() -> None:
 	global JOB_LIST_JOB_STATUS_CHECKBOX_GROUP
 
 	if job_manager.init_jobs(state_manager.get_item('jobs_path')):
-		job_status = get_first(facefusion.choices.job_statuses)
+		job_status = get_first(facfusione.choices.job_statuses)
 
 		JOB_LIST_JOB_STATUS_CHECKBOX_GROUP = gradio.CheckboxGroup(
 			label = wording.get('uis.job_list_status_checkbox_group'),
-			choices = facefusion.choices.job_statuses,
+			choices = facfusione.choices.job_statuses,
 			value = job_status
 		)
 		register_ui_component('job_list_job_status_checkbox_group', JOB_LIST_JOB_STATUS_CHECKBOX_GROUP)
@@ -31,5 +31,5 @@ def listen() -> None:
 
 
 def update_job_status_checkbox_group(job_statuses : List[JobStatus]) -> gradio.CheckboxGroup:
-	job_statuses = job_statuses or facefusion.choices.job_statuses
+	job_statuses = job_statuses or facfusione.choices.job_statuses
 	return gradio.CheckboxGroup(value = job_statuses)
