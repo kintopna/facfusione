cd3772021162a2f2293ecf3063d7541452dabfff
kintopna <kintopna@gmail.com>
2025-08-08 16:00:09 -0500

    Update test_face_analyser.py

--- tests/test_face_analyser.py
+++ tests/test_face_analyser.py
@@ -2,11 +2,11 @@ import subprocess
 
 import pytest
 
-from facefusion import face_classifier, face_detector, face_landmarker, face_recognizer, state_manager
-from facefusion.download import conditional_download
-from facefusion.face_analyser import get_many_faces, get_one_face
-from facefusion.types import Face
-from facefusion.vision import read_static_image
+from facfusione import face_classifier, face_detector, face_landmarker, face_recognizer, state_manager
+from facfusione.download import conditional_download
+from facfusione.face_analyser import get_many_faces, get_one_face
+from facfusione.types import Face
+from facfusione.vision import read_static_image
 from .helper import get_test_example_file, get_test_examples_directory
 
 
