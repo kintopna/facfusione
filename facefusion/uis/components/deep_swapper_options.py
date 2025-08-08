021c4fc7e9f91a91cc4dd58b1694d0a95c616a48
kintopna <kintopna@gmail.com>
2025-08-08 16:56:42 -0500

    Update deep_swapper_options.py

--- facefusion/uis/components/deep_swapper_options.py
+++ facefusion/uis/components/deep_swapper_options.py
@@ -2,12 +2,12 @@ from typing import List, Optional, Tuple
 
 import gradio
 
-from facefusion import state_manager, wording
-from facefusion.common_helper import calc_int_step
-from facefusion.processors import choices as processors_choices
-from facefusion.processors.core import load_processor_module
-from facefusion.processors.types import DeepSwapperModel
-from facefusion.uis.core import get_ui_component, register_ui_component
+from facfusione import state_manager, wording
+from facfusione.common_helper import calc_int_step
+from facfusione.processors import choices as processors_choices
+from facfusione.processors.core import load_processor_module
+from facfusione.processors.types import DeepSwapperModel
+from facfusione.uis.core import get_ui_component, register_ui_component
 
 DEEP_SWAPPER_MODEL_DROPDOWN : Optional[gradio.Dropdown] = None
 DEEP_SWAPPER_MORPH_SLIDER : Optional[gradio.Slider] = None
