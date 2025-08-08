b20b2e3666de6eeb1bc9174dc2d782f9a74830c3
kintopna <kintopna@gmail.com>
2025-08-08 16:06:44 -0500

    Update test_cli_frame_colorizer.py

--- tests/test_cli_frame_colorizer.py
+++ tests/test_cli_frame_colorizer.py
@@ -3,8 +3,8 @@ import sys
 
 import pytest
 
-from facefusion.download import conditional_download
-from facefusion.jobs.job_manager import clear_jobs, init_jobs
+from facfusione.download import conditional_download
+from facfusione.jobs.job_manager import clear_jobs, init_jobs
 from .helper import get_test_example_file, get_test_examples_directory, get_test_jobs_directory, get_test_output_file, is_test_output_file, prepare_test_output_directory
 
 
@@ -27,14 +27,14 @@ def before_each() -> None:
 
 
 def test_colorize_frame_to_image() -> None:
-	commands = [ sys.executable, 'facefusion.py', 'headless-run', '--jobs-path', get_test_jobs_directory(), '--processors', 'frame_colorizer', '-t', get_test_example_file('target-240p-0sat.jpg'), '-o', get_test_output_file('test_colorize-frame-to-image.jpg') ]
+	commands = [ sys.executable, 'facfusione.py', 'headless-run', '--jobs-path', get_test_jobs_directory(), '--processors', 'frame_colorizer', '-t', get_test_example_file('target-240p-0sat.jpg'), '-o', get_test_output_file('test_colorize-frame-to-image.jpg') ]
 
 	assert subprocess.run(commands).returncode == 0
 	assert is_test_output_file('test_colorize-frame-to-image.jpg') is True
 
 
 def test_colorize_frame_to_video() -> None:
-	commands = [ sys.executable, 'facefusion.py', 'headless-run', '--jobs-path', get_test_jobs_directory(), '--processors', 'frame_colorizer', '-t', get_test_example_file('target-240p-0sat.mp4'), '-o', get_test_output_file('test-colorize-frame-to-video.mp4'), '--trim-frame-end', '1' ]
+	commands = [ sys.executable, 'facfusione.py', 'headless-run', '--jobs-path', get_test_jobs_directory(), '--processors', 'frame_colorizer', '-t', get_test_example_file('target-240p-0sat.mp4'), '-o', get_test_output_file('test-colorize-frame-to-video.mp4'), '--trim-frame-end', '1' ]
 
 	assert subprocess.run(commands).returncode == 0
 	assert is_test_output_file('test-colorize-frame-to-video.mp4') is True
