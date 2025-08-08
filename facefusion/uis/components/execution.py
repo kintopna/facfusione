cb4c7fe8d42ec72daa71f4cb0309538f326e2a16
kintopna <kintopna@gmail.com>
2025-08-08 16:56:00 -0500

    Update execution.py

--- facefusion/uis/components/execution.py
+++ facefusion/uis/components/execution.py
@@ -2,11 +2,11 @@ from typing import List, Optional
 
 import gradio
 
-from facefusion import content_analyser, face_classifier, face_detector, face_landmarker, face_masker, face_recognizer, state_manager, voice_extractor, wording
-from facefusion.execution import get_available_execution_providers
-from facefusion.filesystem import get_file_name, resolve_file_paths
-from facefusion.processors.core import get_processors_modules
-from facefusion.types import ExecutionProvider
+from facfusione import content_analyser, face_classifier, face_detector, face_landmarker, face_masker, face_recognizer, state_manager, voice_extractor, wording
+from facfusione.execution import get_available_execution_providers
+from facfusione.filesystem import get_file_name, resolve_file_paths
+from facfusione.processors.core import get_processors_modules
+from facfusione.types import ExecutionProvider
 
 EXECUTION_PROVIDERS_CHECKBOX_GROUP : Optional[gradio.CheckboxGroup] = None
 
@@ -36,7 +36,7 @@ def update_execution_providers(execution_providers : List[ExecutionProvider]) ->
 		face_recognizer,
 		voice_extractor
 	]
-	available_processors = [ get_file_name(file_path) for file_path in resolve_file_paths('facefusion/processors/modules') ]
+	available_processors = [ get_file_name(file_path) for file_path in resolve_file_paths('facfusione/processors/modules') ]
 	processor_modules = get_processors_modules(available_processors)
 
 	for module in common_modules + processor_modules:
