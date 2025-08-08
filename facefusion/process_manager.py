a07cee592da9b275ab33879a9db0dd6b9827a44e
kintopna <kintopna@gmail.com>
2025-08-08 16:17:32 -0500

    Update process_manager.py

--- facefusion/process_manager.py
+++ facefusion/process_manager.py
@@ -1,6 +1,6 @@
 from typing import Generator, List
 
-from facefusion.types import ProcessState, QueuePayload
+from facfusione.types import ProcessState, QueuePayload
 
 PROCESS_STATE : ProcessState = 'pending'
 
