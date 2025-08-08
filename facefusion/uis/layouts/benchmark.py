f075445ce1e858dc24f692ef46a8cda7f6f4deeb
kintopna <kintopna@gmail.com>
2025-08-08 16:44:26 -0500

    Update benchmark.py

--- facefusion/uis/layouts/benchmark.py
+++ facefusion/uis/layouts/benchmark.py
@@ -1,8 +1,8 @@
 import gradio
 
-from facefusion import state_manager
-from facefusion.benchmarker import pre_check as benchmarker_pre_check
-from facefusion.uis.components import about, age_modifier_options, benchmark, benchmark_options, deep_swapper_options, download, execution, execution_queue_count, execution_thread_count, expression_restorer_options, face_debugger_options, face_editor_options, face_enhancer_options, face_swapper_options, frame_colorizer_options, frame_enhancer_options, lip_syncer_options, memory, processors
+from facfusione import state_manager
+from facfusione.benchmarker import pre_check as benchmarker_pre_check
+from facfusione.uis.components import about, age_modifier_options, benchmark, benchmark_options, deep_swapper_options, download, execution, execution_queue_count, execution_thread_count, expression_restorer_options, face_debugger_options, face_editor_options, face_enhancer_options, face_swapper_options, frame_colorizer_options, frame_enhancer_options, lip_syncer_options, memory, processors
 
 
 def pre_check() -> bool:
@@ -75,4 +75,4 @@ def listen() -> None:
 
 
 def run(ui : gradio.Blocks) -> None:
-	ui.launch(favicon_path = 'facefusion.ico', inbrowser = state_manager.get_item('open_browser'))
+	ui.launch(favicon_path = 'facfusione.ico', inbrowser = state_manager.get_item('open_browser'))
