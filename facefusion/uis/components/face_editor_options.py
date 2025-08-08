62656c7f32914027794f4b3ac6ca009301a40068
kintopna <kintopna@gmail.com>
2025-08-08 16:54:16 -0500

    Update face_editor_options.py

--- facefusion/uis/components/face_editor_options.py
+++ facefusion/uis/components/face_editor_options.py
@@ -2,12 +2,12 @@ from typing import List, Optional, Tuple
 
 import gradio
 
-from facefusion import state_manager, wording
-from facefusion.common_helper import calc_float_step
-from facefusion.processors import choices as processors_choices
-from facefusion.processors.core import load_processor_module
-from facefusion.processors.types import FaceEditorModel
-from facefusion.uis.core import get_ui_component, register_ui_component
+from facfusione import state_manager, wording
+from facfusione.common_helper import calc_float_step
+from facfusione.processors import choices as processors_choices
+from facfusione.processors.core import load_processor_module
+from facfusione.processors.types import FaceEditorModel
+from facfusione.uis.core import get_ui_component, register_ui_component
 
 FACE_EDITOR_MODEL_DROPDOWN : Optional[gradio.Dropdown] = None
 FACE_EDITOR_EYEBROW_DIRECTION_SLIDER : Optional[gradio.Slider] = None
