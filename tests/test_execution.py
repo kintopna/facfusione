0304bae977b3999ee52391c3fbd891fa00debf7a
kintopna <kintopna@gmail.com>
2025-08-08 16:00:30 -0500

    Update test_execution.py

--- tests/test_execution.py
+++ tests/test_execution.py
@@ -1,4 +1,4 @@
-from facefusion.execution import create_inference_session_providers, get_available_execution_providers, has_execution_provider
+from facfusione.execution import create_inference_session_providers, get_available_execution_providers, has_execution_provider
 
 
 def test_has_execution_provider() -> None:
