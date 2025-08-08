2daf547b93e3a175c6e88525193c98c1843cfdbf
kintopna <kintopna@gmail.com>
2025-08-08 16:32:03 -0500

    Update audio.py

--- facefusion/audio.py
+++ facefusion/audio.py
@@ -5,10 +5,10 @@ import numpy
 import scipy
 from numpy.typing import NDArray
 
-from facefusion.ffmpeg import read_audio_buffer
-from facefusion.filesystem import is_audio
-from facefusion.types import Audio, AudioFrame, Fps, Mel, MelFilterBank, Spectrogram
-from facefusion.voice_extractor import batch_extract_voice
+from facfusione.ffmpeg import read_audio_buffer
+from facfusione.filesystem import is_audio
+from facfusione.types import Audio, AudioFrame, Fps, Mel, MelFilterBank, Spectrogram
+from facfusione.voice_extractor import batch_extract_voice
 
 
 @lru_cache()
