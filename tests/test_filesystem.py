8bc0aff2628ddf546b708a71a181a7c7a81ba642
kintopna <kintopna@gmail.com>
2025-08-08 15:58:15 -0500

    Update test_filesystem.py

--- tests/test_filesystem.py
+++ tests/test_filesystem.py
@@ -2,8 +2,8 @@ import os.path
 
 import pytest
 
-from facefusion.download import conditional_download
-from facefusion.filesystem import create_directory, filter_audio_paths, filter_image_paths, get_file_extension, get_file_format, get_file_size, has_audio, has_image, has_video, in_directory, is_audio, is_directory, is_file, is_image, is_video, remove_directory, resolve_file_paths, same_file_extension
+from facfusione.download import conditional_download
+from facfusione.filesystem import create_directory, filter_audio_paths, filter_image_paths, get_file_extension, get_file_format, get_file_size, has_audio, has_image, has_video, in_directory, is_audio, is_directory, is_file, is_image, is_video, remove_directory, resolve_file_paths, same_file_extension
 from .helper import get_test_example_file, get_test_examples_directory, get_test_outputs_directory
 
 
